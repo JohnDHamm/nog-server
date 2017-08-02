52ff7c4bf7afefba5e5c79294d6c5007cffa25e7
John Hamm <johndhamm1@gmail.com>
2017-08-02 17:10:07 -0500

    connect to mLab db

--- db/database.js
+++ db/database.js
@@ -1,12 +1,15 @@
-
 'use strict';
 
 const mongoose = require('mongoose');
 
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/nogserver'
+// const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/nogserver'
 const PORT = process.env.PORT || 3000
 
-// const MONGODB_URL = 'mongodb://<dbuser>:<dbpassword>@ds013216.mlab.com:13216/nogserver'
+const { getMLabCreds } = require('../creds/creds');
+// const dbuser = getMLabCreds().dbname;
+// console.log("dbuser", dbuser);
+
+const MONGODB_URL = `mongodb://${getMLabCreds().dbname}:${getMLabCreds().dbpassword}@ds013216.mlab.com:13216/nogserver`
 
 mongoose.Promise = Promise
 
